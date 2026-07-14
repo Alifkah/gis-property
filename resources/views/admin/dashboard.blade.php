9b9298eb771c6c37686eff4af4eb2d3fac0c117c
Alifkah <alifkah46@gmail.com>
2026-07-14 19:51:59 +0800

    fix: Disable button PDF

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,12 +11,6 @@
                 </svg>
                 <span>Ekspor CSV</span>
             </a>
-            <button onclick="window.print()" class="btn btn-primary shadow-sm cursor-pointer">
-                <svg class="size-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
-                    <path d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-                <span>Cetak Laporan</span>
-            </button>
         </div>
     </div>
 
